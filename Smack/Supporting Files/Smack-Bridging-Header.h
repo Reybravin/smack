7c58291a7eb78fadff380faf16afbfd7c65190b7
Sergiy Sachuk <sergiysachuk@Sergiys-MacBook-Pro.local>
2017-11-06 17:35:16 +0200

    initial project setup

---
+++ Smack/Supporting Files/Smack-Bridging-Header.h
@@ -0,0 +1,5 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "SWRevealViewController.h" //gives access to methods and functions of objective-c files
