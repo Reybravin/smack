105c091c985252e2cbf10340284f321b1f7a3f63
Sergiy Sachuk <sergiysachuk@Sergiys-MacBook-Pro.local>
2017-11-09 14:19:14 +0200

    added loginUI

--- Smack/Controller/ChannelVC.swift
+++ Smack/Controller/ChannelVC.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ChannelVC: UIViewController {
 
+    //Outlets
+    @IBOutlet weak var loginBtn: UIButton! //to change the title of the button to the user's name
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
     }
 
+    @IBAction func loginBtnPressed(_ sender: Any) {
+        performSegue(withIdentifier: TO_LOGIN, sender: nil)
+    }
+    
 }
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Sergiy Sachuk on 11/9/17.
+//  Copyright © 2017 Sergiy Sachuk. All rights reserved.
+//
+
+import Foundation
+
+//Segue
+
+let TO_LOGIN = "toLogin"
