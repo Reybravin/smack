6293680d69771458d9c783cf44127505d90f95a1
Sergiy Sachuk <sergiysachuk@Sergiys-MacBook-Pro.local>
2017-11-13 09:44:20 +0200

    lesson 9

--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -8,8 +8,23 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Success : Bool) -> ()
+/*typealias = renaming:
+ typealias Jonny = String
+ let name: Jonny = "Jonny"
+*/
+
+//URL Constants
+let BASE_URL = "https://chattychatss.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
+
 //Segue
 
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWID = "unwindToChannel"
+
+//User Defaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
