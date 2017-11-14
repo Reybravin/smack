2bfade3d6500144f2ec28ca6f6ca0058f6962733
Sergiy Sachuk <sergiysachuk@Sergiys-MacBook-Pro.local>
2017-11-14 15:22:45 +0200

    lesson 12.

--- Smack/Controller/CreateAccountVC.swift
+++ Smack/Controller/CreateAccountVC.swift
@@ -11,10 +11,14 @@ import UIKit
 class CreateAccountVC: UIViewController {
     
     //Outlets
-    @IBOutlet weak var userNaneTxt: UITextField!
+    @IBOutlet weak var userNameTxt: UITextField!
     @IBOutlet weak var emailTxt: UITextField!
     @IBOutlet weak var passTxt: UITextField!
     @IBOutlet weak var userImg: UIImageView!
+    
+    //Default Variables
+    var avatarName = "profileDefault"
+    var avatarColor = "[0.5, 0.5, 0.5, 1]" //default light grey color
     
     
     override func viewDidLoad() {
@@ -25,14 +29,26 @@ class CreateAccountVC: UIViewController {
     @IBAction func createAccntPressed(_ sender: Any) {
         
         //creating variables for emails and passwords
-        guard let email = emailTxt.text , emailTxt.text != "" else{ return } // guard statement is another way unwrapping optional values. "," stands for "where". It is to check if emailTxt.text does not equal to epmty string.
+        // guard statement is another way unwrapping optional values. "," stands for "where". It is to check if emailTxt.text does not equal to epmty string.
+        guard let name = userNameTxt.text, userNameTxt.text != "" else { return }
+        guard let email = emailTxt.text , emailTxt.text != "" else{ return }
         guard let pass = passTxt.text , passTxt.text != "" else { return }
         
         //passing the values to the register user function when we click a Register user btn
         AuthService.instance.registerUser(email: email, password: pass) { (success) in
             
             if success {
-                print("registered user!ß")
+                AuthService.instance.loginUser(email: email, password: pass, completion: { (success) in
+                    if success {
+                        AuthService.instance.createUser(name: name, email: email, avatarName: self.avatarName, avatarColor: self.avatarColor, completion: { (success) in
+                            if success {
+                            print(UserDataService.instance.name, UserDataService.instance.avatarName)
+                               self.performSegue(withIdentifier: UNWIND, sender: nil)
+                            }
+                        })
+                    }
+                    
+                })
             }
         }
     }
@@ -44,7 +60,7 @@ class CreateAccountVC: UIViewController {
     }
     
     @IBAction func closePressed(_ sender: Any) {
-        performSegue(withIdentifier: UNWID, sender: nil)
+        performSegue(withIdentifier: UNWIND, sender: nil)
     }
     
 
--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -17,14 +17,21 @@ typealias CompletionHandler = (_ Success : Bool) -> ()
 //URL Constants
 let BASE_URL = "https://chattychatnew.herokuapp.com/v1/"
 let URL_REGISTER = "\(BASE_URL)account/register"
+let URL_LOGIN = "\(BASE_URL)account/login"
+let URL_USER_ADD = "\(BASE_URL)user/add"
 
 //Segue
 
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
-let UNWID = "unwindToChannel"
+let UNWIND = "unwindToChannel"
 
 //User Defaults
 let TOKEN_KEY = "token"
 let LOGGED_IN_KEY = "loggedIn"
 let USER_EMAIL = "userEmail"
+
+//Headers
+let HEADER = [
+    "Content-Type": "application/json; charset = utf-8"
+]
